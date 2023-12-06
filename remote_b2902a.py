d2c771a4af60ce26473dbdb23cfe30eb8b16c69a
Nikolayb33 <nikolayb33@gmail.com>
2023-12-06 12:51:34 +0300

    version without current limit

--- remote_b2902a.py
+++ remote_b2902a.py
@@ -1,9 +1,10 @@
-from pyvisa import *
+import sys
+from pyvisa import ResourceManager
 from time import sleep, time
-rm = ResourceManager()
-print(rm.list_resources())
-b2902a_1 = rm.list_resources()[0]
-inst = rm.open_resource(b2902a_1)
+# rm = ResourceManager()
+# print(rm.list_resources())
+# b2902a_1 = rm.list_resources()[0]
+# inst = rm.open_resource(b2902a_1)
 # print(inst.query("*IDN?"))
 
 # Spot measurement
@@ -43,22 +44,29 @@ inst = rm.open_resource(b2902a_1)
 
 # Задание постоянного напряжения
 def const_voltage_ch1(voltage, current, count, file_name):
+    rm = ResourceManager()
+    print(rm.list_resources())
+    b2902a_1 = rm.list_resources()[0]
+    inst = rm.open_resource(b2902a_1)
+
     # Spot measurement
+
     inst.write("*RST")
     inst.write("*CLS")
-    inst.write(f":sour1:func:mode {voltage}")
+    inst.write(":sour1:func:mode volt")
     # inst.write(":sour2:func:mode curr")
-    inst.write(f":sens1:curr:rang:prot {current}")
+    inst.write(f":sens1:curr:rang:auto off") 
+    inst.write(f":sens1:curr:rang:lim {current}") # не задается предел по току
     # inst.write(":sens2.volt:rang:auto on")
     inst.write("outp1 on")
     # inst.write("outp2 on")
-    inst.write(":sour1:volt 20")
+    inst.write(f":sour1:volt {voltage}")
     log_file = open(f"{file_name}", "w", encoding="utf-8")
     log_file.write("Время Напряжение\t Ток\n")
     a = time()
 
     print("Напряжение" + "\t" + "Ток")
-    for i in range(1, count):
+    for i in range(1, int(count)):
         obj = {"Время": time() - a,
         "Напряжение":inst.query("meas:volt? (@1)").strip(),
         "Ток":inst.query("meas:curr? (@1)").strip()}
@@ -66,7 +74,7 @@ def const_voltage_ch1(voltage, current, count, file_name):
             obj["Ток"])
         
         log_file.write(f'{obj["Время"]:.4}, {obj["Напряжение"]}, {obj["Ток"]} \n')
-        sleep(1.00)
+        sleep(1.00) # задержка между измерениями
 
 
     inst.write("outp1 off")
@@ -74,3 +82,9 @@ def const_voltage_ch1(voltage, current, count, file_name):
     log_file.close()
     # print(voltage)
     # print(current)
+
+
+if __name__ == "__main__":
+    arg = sys.argv[1:] # срезом убираем название скрипта
+    # print(arg)
+    const_voltage_ch1(*arg) # распаковываем все аргументы в функцию
